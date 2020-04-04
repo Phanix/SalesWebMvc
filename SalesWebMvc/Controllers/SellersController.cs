ed0883ac76993371c796b0733641bcd05caf99c1
Fabio Henrique <fh2game86@gmail.com>
2020-04-04 15:39:51 -0300

    Delete seller

--- SalesWebMvc/Controllers/SellersController.cs
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -39,6 +39,27 @@ namespace SalesWebMvc.Controllers
             _sellerServices.Insert(seller);
             return RedirectToAction(nameof(Index));
         }
+        
+        public IActionResult Delete(int? id)
+        {
+            if(id == null)
+            {
+                return NotFound();
+            }
+            var obj = _sellerServices.FindViewById(id.Value);
+            if(obj == null)
+            {
+                return NotFound();
+            }
+            return View(obj);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            _sellerServices.Remove(id);
+            return RedirectToAction(nameof(Index));
+        }
 
         
     }
