b57af05a6968593af252180bda90d8154f892558
Fabio Henrique <fh2game86@gmail.com>
2020-04-04 12:28:43 -0300

    First commit

---
+++ SalesWebMvc/Controllers/SellersController.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using SalesWebMvc.Models;
+using SalesWebMvc.Services;
+using SalesWebMvc.Models.ViewModels;
+
+namespace SalesWebMvc.Controllers
+{
+    public class SellersController : Controller
+    {
+        private readonly SellerService _sellerServices;
+        private readonly DepartmentService _departmentService;
+
+        public SellersController(SellerService sellerService, DepartmentService departmentService)
+        {
+            _departmentService = departmentService;
+            _sellerServices = sellerService;
+        }
+
+        public IActionResult Index()
+        {
+            var list = _sellerServices.FindAll();
+            return View(list);
+        }
+
+        public IActionResult Create()
+        {
+            var departments = _departmentService.FindAll();
+            var viewModel = new SellerFormViewModel { Departments = departments };
+            return View(viewModel);
+        }
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(Seller seller)
+        {
+            _sellerServices.Insert(seller);
+            return RedirectToAction(nameof(Index));
+        }
+
+        
+    }
+}
---
+++ SalesWebMvc/Services/SellerService.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using SalesWebMvc.Data;
+using SalesWebMvc.Models;
+namespace SalesWebMvc.Services
+{
+    public class SellerService
+    {
+        private readonly SalesWebMvcContext _context;
+
+        public SellerService(SalesWebMvcContext context)
+        {
+            _context = context;
+        }
+
+        public List<Seller> FindAll()
+        {
+            //Acessa tabela vendedor e return um lista 
+            return _context.Seller.ToList();
+        }
+        public void Insert(Seller seller)
+        {           
+            _context.Add(seller);
+            _context.SaveChanges();
+        }
+    }
+}
