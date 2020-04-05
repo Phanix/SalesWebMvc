63d555ce5d67ac74ccc4daf75184519e532070fb
Fabio Henrique <fh2game86@gmail.com>
2020-04-05 02:29:46 -0300

    Asynchronous operations using Tasks (async, await)

--- SalesWebMvc/Services/SellerService.cs
+++ SalesWebMvc/Services/SellerService.cs
@@ -17,31 +17,32 @@ namespace SalesWebMvc.Services
             _context = context;
         }
 
-        public List<Seller> FindAll()
+        public async  Task<List<Seller>> FindAllAsync()
         {
-            //Acessa tabela vendedor e return um lista 
-            return _context.Seller.ToList();
+            //Acessa tabela vendedor e return uma lista 
+            return await _context.Seller.ToListAsync();
         }
-        public void Insert(Seller seller)
+        public async Task InsertAsync(Seller seller)
         {           
             _context.Add(seller);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
-        public Seller FindViewById(int id)
+        public async Task<Seller> FindViewByIdAsync(int id)
         {
-            return _context.Seller.Include(seller => seller.Department).FirstOrDefault(seller => seller.Id == id);
+            return await _context.Seller.Include(seller => seller.Department).FirstOrDefaultAsync(seller => seller.Id == id);
         }
 
-        public void Remove(int id)
+        public async Task RemoveAsync(int id)
         {
-            var obj = _context.Seller.Find(id);
+            var obj = await _context.Seller.FindAsync(id);
             _context.Seller.Remove(obj);
-            _context.SaveChanges();
+            await _context.SaveChangesAsync();
         }
 
-        public void Update(Seller seller)
+        public async Task UpdateAsync(Seller seller)
         {
-            if(!_context.Seller.Any(x => x.Id == seller.Id))
+            bool hasAny = await _context.Seller.AnyAsync(x => x.Id == seller.Id);
+            if (!hasAny)
             {
                 throw new NotFoundException("Id not found");
             }
@@ -50,7 +51,7 @@ namespace SalesWebMvc.Services
                 //Update the object
                 _context.Update(seller);
                 //Update the context
-                _context.SaveChanges();
+               await _context.SaveChangesAsync();
             }catch(DbUpdateConcurrencyException e)
             {
                 throw new DbConcurrencyException(e.Message);
